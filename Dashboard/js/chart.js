0e44114ebe81f16a82822565c6b6cdabbab4f805
Meara <mearaleentvaar@gmail.com>
2022-06-11 22:22:53 +0200

    Dashboard Icon en design Drag&Drop
    Eigen icoon geupload voor het dashboard. Eigen visuals geupload voor het drag & Drop poc

--- Dashboard/js/chart.js
+++ Dashboard/js/chart.js
@@ -1,14 +1,16 @@
 const ctx = document.getElementById('myChart').getContext('2d');
+let week = [];
+let delayed;
 fetchJSON();
 const myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-        labels: ['.', '.', '.', '.', '.', '.', '.', '.'],
+        labels:[], // Deze puntjes moeten er staan anders wordt de data niet ingeladen. 
         datasets: [{
-            label: '',
+            label: 'CSAT Percentage',
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)', // Achtergrond kleur van de bars
                 'rgba(54, 162, 235, 0.2)',
 
             ],
@@ -24,13 +26,24 @@ const myChart = new Chart(ctx, {
 
     },
     options: {
+        scales:{
+            // x:{
+            //     display:false
+            // },
+            // y:{
+            //     display:false
+            // },
+        },
+        animation: {
+            duration: 0,
+        },
         plugins: {
             legend: {
                 display: false
             },
             title: {
                 display: true,
-                text: 'CSAT Percentage per week'
+                text: 'CSAT Percentage per week' // Voegt titel toe aan de top
             },
 
 
@@ -41,24 +54,21 @@ const myChart = new Chart(ctx, {
 
 
 async function fetchJSON() {
-    const response = await fetch('test2.json');
-    const data = await response.json();
-    console.log(data);
-    return data;
+    const response = await fetch('test2.json'); // Pakt het bestand test2.json
+    const data = await response.json(); // Wacht op de reactie dat de data binnen is. 
+    return data; // Krijg de data terug
 }
 fetchJSON().then(data => {
-    const week = data.chartdata.map(function (index) {
+    const week = data.chartdata.map(function (index) { // Zoekt in de chartdata van de test2.json en pakt de week. 
         return index.week;
-    });
+    })
     myChart.config.data.labels = week;
-    myChart.config.data.label = week;
     myChart.update();
 });
 
 fetchJSON().then(data => {
     const csat = data.chartdata.map(function (index) {
         return index.csat;
-
     });
     myChart.config.data.datasets[0].data = csat;
     myChart.update();
--- Dashboard/js/main.js
+++ Dashboard/js/main.js
@@ -1,52 +1,25 @@
-
-
-
-
-
 ///////////////////////////  DATA FOR CARDS /////////////////////////
 async function fetchJSON2() {
-    const response = await fetch('dummydata.json');
-    const datapoints = await response.json();
+    const response = await fetch('dummydata.json'); // haalt de dummydata.json op
+    const datapoints = await response.json(); // wacht op reactie
     console.log(datapoints);
-    return datapoints;
+    return datapoints; // krijgt de data terug
 }
 
 fetchJSON2().then(datapoints => {
       const team = datapoints.testData.map(function (index) {
-     return index.rankingteam;
+     return index.rankingteam; // haalt de rankingteam op
       })
       const id = datapoints.testData.map(function (index) {
-        return index.rankingind
+        return index.rankingind // haalt de rankingind op
          })
-      document.getElementById("number2").innerHTML ="#"+ team[6];
-      document.getElementById("number3").innerHTML ="#"+ id[8];
-    const klanten = datapoints.testData.map(testData => testData.klanten).reduce((test, testData) => testData + test);
-    console.log(klanten);
-    document.getElementById("number1").innerHTML = klanten;
+      document.getElementById("number2").innerHTML ="#"+ team[team.length-1]; // Voegt in de html met id number2 het laatste team ranking op. 
+      document.getElementById("number3").innerHTML ="#"+ id[id.length-1]; // Voegt in de html met id number3 het laatste id ranking op. 
+    const klanten = datapoints.testData.map(testData => testData.klanten).reduce((test, testData) => testData + test); // haalt alles van het eerste getal in de array, vervolgens telt hij alles weer bij elkaar op.
+    document.getElementById("number1").innerHTML = klanten; // voegt de aantal klanten toe in de html met id number1
 
     // const klanten = datapoints.testData.map(function (index) {
     //   return index.klanten;
 });
 // document.getElementById("number1").innerHTML = klanten[2];
 
-async function fetchJSON() {
-    const response = await fetch('test2.json');
-    const data = await response.json();
-    console.log(data);
-    return data;
-}
-fetchJSON().then(data => {
-    const week = data.chartdata.map(function (index) {
-      return index.week;
-    });
-    myChart.config.data.labels =  week;
-    myChart.update();
-  });
-
-  fetchJSON().then(data => {
-    const csat = data.chartdata.map(function (index) {
-      return index.csat;
-    });
-    myChart.config.data.datasets[0].data = csat;
-    myChart.update();
-  });
